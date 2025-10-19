dda6fcf9a5c4b74e1269d3cd81dfbe51b8deec0c
Gianca0204 <gian.mrwolfango@gmail.com>
2025-10-19 12:26:23 +0200

    Implement API handler to fetch product data

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      'http://api.selectmoda.com:85/api/Producto/GetProductos?login=BRITISHOP&TEMPORADA=Q2%202025',
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImYzYjJmNzMzLWVhOTUtNGYxZC05MDk3LWY0M2IzZTY2MmMwMyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJCUklUSVNIT1AiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImZkZTNiNTVjLTUwMGEtNGI3MS1hYWQyLThkMWUxZTU4ZTlhMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6Im9jdC4gbHUuIDIwIDIwMjUgMDk6NTE6MDIgIiwibmJmIjoxNzYwODY3NDYyLCJleHAiOjE3NjA5NDY2NjIsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjcxODgiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo3MTg4In0.NLW5IZ8UoycuT63bdPgxbxY1kb-pvg2waNwksd6AaGU',
+          'Accept': 'text/plain'
+        },
+      }
+    );
+
+    const data = await response.text();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
